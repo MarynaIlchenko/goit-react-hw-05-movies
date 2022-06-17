b885582710001e20c635592f0005da9fa9cf7f31
MarynaIlchenko <debovamail@gmail.com>
2022-06-18 02:17:33 +0300

    update

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,9 @@
 import { Link } from 'react-router-dom';
 import styles from './Navigation.module.css';
 
-const Navigation = () => (
+export const Navigation = () => (
   <nav>
-    <Link to="/home">Home</Link>
+    <Link to="/">Home</Link>
     <Link to="/movies">Movies</Link>
   </nav>
 );
-export default Navigation;
